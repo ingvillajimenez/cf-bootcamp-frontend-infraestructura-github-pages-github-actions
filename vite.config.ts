0c8bb5e47ed9b319eb5b109841fa3bbf780e5843
Juan Villa <ing.villa.jimenez@gmail.com>
2025-05-27 23:31:36 -0600

    Quitar base en vite.config.ts para Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/cf-bootcamp-frontend-infraestructura-github-pages-github-actions/",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
