5583a71e33e391c6ce3f7134bfd5235bfd4e75e4
Juan Villa <ing.villa.jimenez@gmail.com>
2025-05-27 23:56:52 -0600

    Deploy to GitHub Pages gh-pages branch

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/cf-bootcamp-frontend-infraestructura-github-pages-github-actions/",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
