f4f2b3823936c1d99077519f115d51188c0026dc
Juan Villa <ing.villa.jimenez@gmail.com>
2025-05-27 19:49:24 -0600

    Escribiendo primer GitHub Action para desplegar en GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/cf-bootcamp-frontend-infraestructura-github-pages-github-actions/",
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
